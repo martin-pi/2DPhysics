102ecb956ea0263d968d88bf05637f6d9ab76807
John Hewitt <hewittjd@appstate.edu>
2018-02-07 16:55:01 -0500

    Fixed Headless Mode

--- engine/src/main/java/in/patrickmart/App.java
+++ engine/src/main/java/in/patrickmart/App.java
@@ -14,9 +14,10 @@ public class App
         System.out.println( "Hello World!" );
         Model m = new Model();
         Controller c = new Controller(m);
-        if(args.length > 1 && args[1].equals("headless")) {
+        if(args.length >= 1 && args[0].equals("headless")) {
             System.out.println("Headless Mode");
         } else {
+            System.out.println(args[0]);
             View view = new View(c, m);
             view.runTest();
         }
