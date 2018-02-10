5173d1bf6b29104da7d6a735df6bd5b69560b1dc
Patrick Martin <pharaoh439@gmail.com>
2018-02-10 15:20:41 -0500

    Still working, looking up implementations of XNA for the controller.

---
+++ engine/src/main/java/in/patrickmart/test/ControllerEvent.java
@@ -0,0 +1,11 @@
+package in.patrickmart.test;
+
+public enum ControllerEvent {
+    LOAD_SCENARIO,
+    SAVE_SCENARIO,
+    PLAY_SCENARIO,
+    STOP_SCENARIO,
+    CREATE_ENTITY,
+    REMOVE_ENTITY,
+    SEND_SIGNAL
+}
--- engine/src/main/java/in/patrickmart/test/ControllerTest.java
+++ engine/src/main/java/in/patrickmart/test/ControllerTest.java
@@ -1,5 +1,34 @@
 package in.patrickmart.test;
 
 public class ControllerTest {
-    private ModelTest m;
+    private ModelTest model;
+
+    public ControllerTest(ModelTest model) {
+        this.model = model;
+    }
+
+    public void event(ControllerEvent e) {
+        switch (e) {
+            case LOAD_SCENARIO:
+
+                break;
+            case SAVE_SCENARIO:
+
+                break;
+            case PLAY_SCENARIO:
+
+                break;
+            case STOP_SCENARIO:
+
+                break;
+            case CREATE_ENTITY:
+
+                break;
+            case REMOVE_ENTITY:
+
+                break;
+            default:
+                System.out.println("Invalid event sent to controller.");
+        }
+    }
 }
